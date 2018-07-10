a44f07eae40941a9a3f31b1826f5a6e4717bdcb2
ChrisVonVolotor <evilchris9@gmail.com>
2018-07-10 11:44:37 +0100

    First Commit

--- src/com/qa/app/Account.java
+++ src/com/qa/app/Account.java
@@ -1,16 +1,9 @@
 package com.qa.app;
 
-import com.qa.app.account.management.AccountService;
-
 public class Account {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World to the Standard Out");
-		AccountService service = new AccountService();
-		service.addAccountToMap("bob", "ross");
-		service.addAccountToMap("ross", "ross");
-
-		System.out.println(service.getCreatedAccount(1));
 	}
 
 }
